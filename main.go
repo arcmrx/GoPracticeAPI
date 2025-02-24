367d892f24270c1f02c801c7a2a2b6dc4afe0360
arcmrx <arturvolkov44@gmail.com>
2025-02-24 19:47:23 +0300

    Add PATCH and DELETE handlers

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +13,7 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 		var tasks []Task
 		DB.Find(&tasks)
 		w.Header().Set("Content-Type", "application/json") // Установка формата
-		json.NewEncoder(w).Encode(tasks) // Декодирование 
+		json.NewEncoder(w).Encode(tasks)                   // Декодирование
 	} else {
 		fmt.Fprintln(w, "Only GET")
 	}
@@ -30,6 +31,33 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		vars := mux.Vars(r)
+		taskID := vars["id"]
+		var task Task
+		DB.First(&task, taskID) // поиск задачи по ключу
+		var update map[string]interface{}
+		json.NewDecoder(r.Body).Decode(&update)
+		DB.Model(&task).Updates(update) // Обновление нескольких полей
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(task)
+	} else {
+		fmt.Fprintln(w, "Only PATCH")
+	}
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodDelete {
+		vars := mux.Vars(r)
+		taskID := vars["id"]
+		var task Task
+		DB.Delete(&task, taskID)
+	} else {
+		fmt.Fprintln(w, "Only DELETE")
+	}
+
+}
 func main() {
 	InitDB()
 	DB.AutoMigrate(&Task{})
@@ -37,5 +65,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/get", GetHandler).Methods("GET")
 	router.HandleFunc("/post", PostHandler).Methods("POST")
+	router.HandleFunc("/patch/{id}", PatchHandler).Methods("PATCH")
+	router.HandleFunc("/delete/{id}", DeleteHandler).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
